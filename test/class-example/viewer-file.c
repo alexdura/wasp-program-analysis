a4be01293e465021b3703fcef11ff693e3a35560
Noric Couderc <noric.couderc@cs.lth.se>
2019-11-01 14:28:16 +0100

    Added comments to example files

--- test/class-example/viewer-file.c
+++ test/class-example/viewer-file.c
@@ -1,3 +1,5 @@
+// This file is where we define the viewer-file class 
+// used in class-example.c
 #include <glib.h>
 #include <glib-object.h>
 #include "viewer-file.h"
@@ -9,20 +11,32 @@ typedef struct {
   int x;
 } ViewerFilePrivate;
 
+// We create a type, where we give:
+// - the name we want to give it 
+// - the name we want to give it (again) but in lowecase with '_' (???!?)
+// - The GType of parent type
+// I suspect the second argument is for generating function names
+// talking about the type you are defining.
 G_DEFINE_TYPE_WITH_PRIVATE (ViewerFile, viewer_file, G_TYPE_OBJECT)
 
+// We declare one method here
 static void
 viewer_file_open_impl(ViewerFile *self, GError **error) {
   g_print("VIEWER_FILE: Print file opened\n");
 }
 
+// We declare a contructor here
+// This constructor is for the CLASS object
 static void
 viewer_file_class_init (ViewerFileClass *klass)
 {
+  // Assign viewer_file_open_impl
+  // to "open" in the class object 
   klass->open = viewer_file_open_impl;
   g_print("Viewer file class initialized\n");
 }
 
+// This is a constructor for OBJECTS of the type ViewerFile
 static void
 viewer_file_init (ViewerFile *self)
 {
@@ -33,6 +47,9 @@ viewer_file_init (ViewerFile *self)
   g_print("Viewer file object initialized\n");
 }
 
+// Re-implementation of the method, which will call the 
+// class version. 
+// Each object asks their class what to do.
 void viewer_file_open(ViewerFile *self, GError **error) {
   ViewerFileClass *klass;
   klass = VIEWER_FILE_GET_CLASS (self);
--- test/class-example/viewer-pink.c
+++ test/class-example/viewer-pink.c
@@ -1,19 +1,30 @@
+// This is a file for the class ViewerPink which
+// is used in class-example.c
 #include <glib.h>
 #include <glib-object.h>
 #include "viewer-pink.h"
 
+// We define the type with three args:
+// - Name of the type in camelcase (for values with that type)
+// - Name of the type in lower_case (I think, for generating some functions with the type name)
+// - The parent type.
 G_DEFINE_TYPE(ViewerPink, viewer_pink, VIEWER_TYPE_FILE)
 
+// We declare the function make_it_pink
+// for the CLASS object for ViewerPinks
 static void
 viewer_pink_make_it_pink_impl(void) {
   g_print("VIEWER_PINK: made it pink\n");
 }
 
+// We declare the function open
+// for the CLASS object for ViewerPinks
 static void
 viewer_pink_open_impl(ViewerFile *self, GError **error) {
   g_print("VIEWER_PINK: Pink viewer opened\n");
 }
 
+// Constructor for the class object.
 static void
 viewer_pink_class_init(ViewerPinkClass *klass) {
   klass->make_it_pink = viewer_pink_make_it_pink_impl;
@@ -21,11 +32,13 @@ viewer_pink_class_init(ViewerPinkClass *klass) {
   g_print("Viewer pink class initialized\n");
 }
 
+// Constructor for the ViewerPink objects
 static void
 viewer_pink_init (ViewerPink *self) {
   g_print("Viewer pink object initialized\n");
 }
 
+// make_it_pink method for ViewerPink objects.
 void viewer_make_it_pink(ViewerPink *self) {
   ViewerPinkClass *klass = VIEWER_PINK_GET_CLASS (self);
   klass->make_it_pink();
